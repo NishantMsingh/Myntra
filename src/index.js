25dfc78d6d3ecf917f4ca4f00d9a04edee49d93c
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-16 13:31:09 +0530

    try

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './Main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './store/store';
 import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <Provider store={store}>
     <App />
