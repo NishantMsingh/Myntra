c62221ce3c1647abf7221d5680e3dcf6489c6d68
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-17 20:54:09 +0530

    done

--- src/components/Cart/PaymentTypes/PaymentModes.js
+++ src/components/Cart/PaymentTypes/PaymentModes.js
@@ -1,42 +1,133 @@
-import React from 'react';
-import {PiShootingStarFill} from "react-icons/pi"
-import {MdMoney} from "react-icons/md"
-import {SiFlipkart} from "react-icons/si"
-import {BsCreditCard} from "react-icons/bs"
-import {FaGooglePay} from "react-icons/fa"
-import {BsWallet} from "react-icons/bs"
-import {BsBank} from "react-icons/bs"
+import React, { useState } from "react";
+import { PiShootingStarFill } from "react-icons/pi";
+import { MdMoney } from "react-icons/md";
+import { SiFlipkart } from "react-icons/si";
+import { BsCreditCard } from "react-icons/bs";
+import { FaGooglePay } from "react-icons/fa";
+import { BsWallet } from "react-icons/bs";
+import { BsBank } from "react-icons/bs";
+
 const PaymentModes = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleRadioClick = (option) => {
+    setSelectedOption(option);
+    console.log(option);
+  };
   return (
-    <div className='control-offer '>
+    <div className="control-offer ">
       <div className="row justify-content-center align-items-start">
         <div className="col-lg-6 col-md-6 col-sm-6 ">
-          <button className='w-100 payment-button active text-start'><PiShootingStarFill/> Recomanded</button>
-          <button className='w-100 payment-button text-start'><SiFlipkart/> Flipkart pay later</button>
-          <button className='w-100 payment-button text-start'> <MdMoney/> Cash On Delivery (Cash/UPI)</button>
-          <button className='w-100 payment-button text-start'> <BsCreditCard/> Credit/Debit Card</button>
-          <button className='w-100 payment-button text-start'> <FaGooglePay/> PhonePay / GooglePay/BHIM UPI</button>
-          <button className='w-100 payment-button text-start'> <BsWallet/> Paytm/Wallets</button>
-          <button className='w-100 payment-button text-start'> <BsBank/> Net Banking</button>
-          <button className='w-100 payment-button text-start'> <SiFlipkart/> EMI / Pay Later</button>
+          <button className="w-100 payment-button active text-start">
+            <PiShootingStarFill /> Recomanded
+          </button>
+          <button className="w-100 payment-button text-start">
+            <SiFlipkart /> Flipkart pay later
+          </button>
+          <button className="w-100 payment-button text-start">
+            {" "}
+            <MdMoney /> Cash On Delivery (Cash/UPI)
+          </button>
+          <button className="w-100 payment-button text-start">
+            {" "}
+            <BsCreditCard /> Credit/Debit Card
+          </button>
+          <button className="w-100 payment-button text-start">
+            {" "}
+            <FaGooglePay /> PhonePay / GooglePay/BHIM UPI
+          </button>
+          <button className="w-100 payment-button text-start">
+            {" "}
+            <BsWallet /> Paytm/Wallets
+          </button>
+          <button className="w-100 payment-button text-start">
+            {" "}
+            <BsBank /> Net Banking
+          </button>
+          <button className="w-100 payment-button text-start">
+            {" "}
+            <SiFlipkart /> EMI / Pay Later
+          </button>
         </div>
         <div className="col-lg-6 col-md-6 col-sm-6 pt-3 ">
-          <h5 className='slate '>Recommanded Payment Options</h5>
-          <div className="form-check bg-myntra-light rounded align-items-center d-flex pointer mt-2 p-3">
-  <input className="form-check-input ms-1 me-1 pointer" type="radio" name="flexRadioDefault1" id="flexRadioDefault1"/>
-  <label className="form-check-label  fs-4 slate pointer" htmlFor="flexRadioDefault1">
-   COD
-  </label>
-</div>
-<div className="form-check bg-myntra-light rounded align-items-center d-flex pointer mt-2 p-3">
-  <input className="form-check-input ms-1 me-1 pointer " type="radio" name="flexRadioDefault1" id="flexRadioDefault2" />
-  <label className="form-check-label fs-4 slate pointer" htmlFor="flexRadioDefault2">
-   GooglePay
-  </label>
-</div>
-          </div>  
+          <h5 className="slate ">Recommended Payment Options</h5>
+          <div
+            className={`bg-myntra-light rounded ${
+              selectedOption === "COD" ? "" : "pay-hight"
+            }`}
+          >
+            <div
+              className={`form-check rounded align-items-center d-flex pointer mt-2 p-1`}
+             
+            >
+              <input
+                className="form-check-input ms-1 me-1 pointer"
+                type="radio"
+                name="flexRadioDefault1"
+                id="flexRadioDefault1"
+                onClick={() => handleRadioClick("COD")}
+              />
+              <label
+                className="form-check-label  fs-6 slate pointer"
+                htmlFor="flexRadioDefault1"
+                onClick={() => handleRadioClick("COD")}
+              >
+                Cash on delivery (Cash/Upi) <MdMoney />
+              </label>
+              <br />
+            </div>
+            <div>
+              <p className="fs-6 ms-1">
+                Rs 10 will get charged extra for the Pay on Delivery. You can
+                pay via cash/upi while delivery
+              </p>
+              <button className="slate boldCart empty-button p-1  pb-2 w-90">
+                {" "}
+                PLACE ORDER
+              </button>
+            </div>
+          </div>
+          <div
+            className={`bg-myntra-light rounded mt-2 ${
+              selectedOption === "GooglePay" ? "" : "pay-hight"
+            }`}
+          >
+            <div
+              className={`form-check align-items-center d-flex pointer mt-2 p-3`}
+            
+            >
+              <input
+                className="form-check-input ms-1 me-1 pointer "
+                type="radio"
+                name="flexRadioDefault1"
+                id="flexRadioDefault2"
+                onClick={() => handleRadioClick("GooglePay")}
+              />
+              <label
+                className="form-check-label fs-6 slate pointer"
+                htmlFor="flexRadioDefault2"
+                onClick={() => handleRadioClick("GooglePay")}
+              >
+                GooglePay
+              </label>
+            </div>
+            <div>
+              <p className="fs-6 ms-1 text-light">
+              <a href='upi://nishantkumarsingh16019@okhdfcbank'>
+              Pay & submit : nishantkumarsingh16019@okhdfc<br/>bank
+              </a>
+              </p>
+              <input type='text' placeholder="Transaction id" className="w-90 ms-1 mb-1"/> 
+              <button className="slate boldCart empty-button ps-2 pe-2  ms-1 mb-3 w-90">
+                {" "}
+                PLACE ORDER
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
 export default PaymentModes;
