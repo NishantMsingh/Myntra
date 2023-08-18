868ae2d4cb951a7561e1b271bd117f8bc4666dfc
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-18 23:54:10 +0530

    address validation

--- src/components/Cart/AddressModal.js
+++ src/components/Cart/AddressModal.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "./AddressModal.css";
 import Modal from 'react-bootstrap/Modal';
 import PropTypes from 'prop-types';
-
 const AddressModal = (props) => {
   return (
     <Modal
@@ -11,43 +10,34 @@ const AddressModal = (props) => {
       aria-labelledby=""
       centered
     >
-     
-        
-         
-     
- 
       <Modal.Body>
-      
+
         <div className="">
-        <span className='cartBold h4'>ADD ADDRESS</span>
+          <span className='cartBold h4'>ADD ADDRESS</span>
           <h6 className=" cartBold">Contact details</h6>
-           <div className='d-flex flex-row'>
-          
-          <input className='input' type='text' placeholder='Name....' id='name'/>     <input className='input' type='number' placeholder='Phone' id='number'/> 
-           </div>
-         
+          <div className='d-flex flex-row'>
+
+            <input className='input' type='text' placeholder='Name....' id='name' />     <input className='input' type='number' placeholder='Phone' id='number' />
+          </div>
+
         </div>
         <div className="">
-          <h6  className=" cartBold">Address</h6>
-          
+          <h6 className=" cartBold">Address</h6>
+
           <div className='d-flex flex-row'>
-          <input className='input' type='number' placeholder='Pin code'/>    <input className='input' type='text' placeholder='Locality/town'/> 
-           </div>
-         
-           <div>
-          
-           <textarea className='input w-100' type='text' placeholder='Address...'/> 
-           </div>
-           <button className='place-order-btn w-100' onClick={props.addressHandler}>Proceed to payment</button>
+            <input className='input' type='number' placeholder='Pin code' />    <input className='input' type='text' placeholder='Locality/town' />
+          </div>
+
+          <div>
+
+            <textarea className='input w-100' type='text' placeholder='Address...' />
+          </div>
+          <button className=' cartBold place-order-btn w-100' onClick={props.addressHandler}> ADD ADDRESS</button>
         </div>
       </Modal.Body>
-      
-        
-     
     </Modal>
   );
 }
-
 AddressModal.propTypes = {
   show: PropTypes.bool.isRequired,
   onHide: PropTypes.func.isRequired,
