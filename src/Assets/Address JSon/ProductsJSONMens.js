a43a04be7b3822aa964eb0298c22e3ffd0a9ff89
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-08-18 20:50:53 +0530

    adding products

--- src/Assets/Address JSon/ProductsJSONMens.js
+++ src/Assets/Address JSon/ProductsJSONMens.js
@@ -19,6 +19,25 @@ import productmen41 from "../Images/productmen41.png";
 import product4menImageCard from "../Images/productmen41.png";
 import productmen42 from "../Images/productmen42.png";
 
+import productmen51 from "../Images/productmen51.png";
+import product5menImageCard from "../Images/productmen51.png";
+import productmen52 from "../Images/productmen52.png";
+
+import productmen61 from "../Images/productmen61.png";
+import product6menImageCard from "../Images/productmen61.png";
+import productmen62 from "../Images/productmen62.png";
+
+import productmen71 from "../Images/productmen71.png";
+import product7menImageCard from "../Images/productmen71.png";
+import productmen72 from "../Images/productmen72.png";
+
+import productmen81 from "../Images/productmen81.png";
+import product8menImageCard from "../Images/productmen81.png";
+import productmen82 from "../Images/productmen82.png";
+
+import productmen91 from "../Images/productmen91.png";
+import product9menImageCard from "../Images/productmen91.png";
+import productmen92 from "../Images/productmen92.png";
 const products = [
   {
     productCode: "22834606",
@@ -38,7 +57,7 @@ const products = [
     title: "HRX by Hrithik Roshan",
     description: "Men White Printed Bio-Wash Lifestyle Tshirts",
     star: "4",
-    rating: "978 Ratings",
+    rating: "978 ",
     curPrice: "475",
     prevPrice: "699",
     offer: "32% OFF",
@@ -51,7 +70,7 @@ const products = [
     title: "FORCE IX",
     description: "Pure Cotton Regular Casual T-shirt",
     star: "4",
-    rating: "978 Ratings",
+    rating: "978 ",
     curPrice: "1189",
     prevPrice: "1699",
     offer: "30% OFF",
@@ -64,7 +83,7 @@ const products = [
     title: "FORCE IX",
     description: "Men Pure Cotton Typography Printed T-shirt",
     star: "4.3",
-    rating: "91 Ratings",
+    rating: "91 ",
     curPrice: "888",
     prevPrice: "1999",
     offer: "30% OFF",
@@ -72,6 +91,71 @@ const products = [
     imageCard: product4menImageCard,
     images: [productmen41, productmen42],
   },
+  {
+    productCode: "2340648",
+    title: "Roadster",
+    description: "Time Travlr Men Teal Blue Printed Cotton Pure Cotton T-shirt",
+    star: "3",
+    rating: "6K ",
+    curPrice: "599",
+    prevPrice: "999",
+    offer: "NA",
+    seller: "Savadika Retail Private Limited",
+    imageCard: product5menImageCard,
+    images: [productmen51, productmen52],
+  },
+  {
+    productCode: "1894413",
+    title: "Wrong",
+    description: "Men Grey Melange Self Design Welt Pocket T-shirt",
+    star: "3",
+    rating: "6K ",
+    curPrice: "713",
+    prevPrice: "1299",
+    offer: "23% off",
+    seller: "Flash star commerce",
+    imageCard: product6menImageCard,
+    images: [productmen61, productmen62],
+  },
+  {
+    productCode: "18786234",
+    title: "Louis Philippe",
+    description: "Men Beige & White Slim Fit Striped Single-Breasted Formal Blazer",
+    star: "3.9",
+    rating: "1.2K ",
+    curPrice: "4129",
+    prevPrice: "6999",
+    offer: "23% off",
+    seller: "Westbury Holdings Pvt Ltd_Madura",
+    imageCard: product7menImageCard,
+    images: [productmen71, productmen72],
+  },
+  {
+    productCode: "20931896",
+    title: "VASTRAMAY",
+    description: "Men Black Slim-Fit Textured Sherwani Set",
+    star: "3.9",
+    rating: "2K ",
+    curPrice: "3940",
+    prevPrice: "7298",
+    offer: "46% OFF",
+    seller: "Vastramay Pvt. Ltd",
+    imageCard: product8menImageCard,
+    images: [productmen81, productmen82],
+  },
+  {
+    productCode: "20921890",
+    title: "The Indian Garage Co",
+    description: "Men Checked Pure Cotton Casual Shirt",
+    star: "4.6",
+    rating: "364 ",
+    curPrice: "725",
+    prevPrice: "2500",
+    offer: "67% OFF",
+    seller: "THE INDIAN GARAGE CO.",
+    imageCard: product9menImageCard,
+    images: [productmen91, productmen92],
+  }
  
 ];
 
